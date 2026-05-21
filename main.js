79cdfad8ead85091980db53fd999241a89d0aebd
nautimon <nautisignup@gmail.com>
2026-05-21 20:35:48 +0000

    Calculate total bill

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+//My Code
+function calculateBillTotal(subtotal){
+    var tax = subtotal * 0.095;
+    var tip = subtotal * 0.2;
+    var total = subtotal + tip + tax;
+    return total;
+}
 
+console.log(calculateBillTotal(20))
